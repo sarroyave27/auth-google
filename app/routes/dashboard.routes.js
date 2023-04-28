60f96b983dc3967cc6c1c9e5e469455edd46fa5a
kevinalg8 <kalondono35@soy.sena.edu.co>
2023-04-28 17:27:05 -0500

    datos

--- app/routes/dashboard.routes.js
+++ app/routes/dashboard.routes.js
@@ -9,8 +9,10 @@ dash.get("/inicio", (req, res) => {
     if (req.cookies.ckswf){
     try {
         const token = jwt.verify(req.cookies.ckswf, process.env.SECRET_KEY);
-
-        res.render("dash");
+        res.render("dash", {
+            "nombre":token.nombre,
+            "foto":token.foto
+        });
 
     } catch (error) {
         res.redirect("/");
